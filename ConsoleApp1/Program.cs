5a557645ef33368448802fb6ab89276266ae9884
aniaania321 <s31154@pjwstk.edu.pl>
2025-03-26 21:33:27 +0100

    I looked through to find violations of interface segregation principle but I think the previous point (adding turnDevices intreface) fixed that so I am moving on to dependency inversion principle

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Tutorial3_Task;
+
+class Program
+{
+    public static void Main()
+    {
+        
+        try
+        {
+            FileService fileService = new("/Users/aniasmuga/RiderProjects/Task3_APBD/ConsoleApp1/input.txt");
+            DeviceManager deviceManager = new(fileService);
+            
+            Console.WriteLine("Devices presented after file read.");
+            deviceManager.ShowAllDevices();
+            
+            Console.WriteLine("Create new computer with correct data and add it to device store.");
+            {
+                PersonalComputer computer = new("P-2", "ThinkPad T440", false, null);
+                deviceManager.AddDevice(computer);
+            }
+            
+            Console.WriteLine("Let's try to enable this PC");
+            try
+            {
+                deviceManager.TurnOnDevice("P-2");
+            }
+            catch (EmptySystemException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.WriteLine("Let's install OS for this PC");
+            
+            PersonalComputer editComputer = new("P-2", "ThinkPad T440", true, "Arch Linux");
+            deviceManager.EditDevice(editComputer);
+            
+            Console.WriteLine("Let's try to enable this PC");
+            deviceManager.TurnOnDevice("P-2");
+            
+            Console.WriteLine("Let's turn off this PC");
+            deviceManager.TurnOffDevice("P-2");
+            
+            Console.WriteLine("Delete this PC");
+            deviceManager.RemoveDeviceById("P-2");
+            
+            Console.WriteLine("Devices presented after all operations.");
+            deviceManager.ShowAllDevices();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
